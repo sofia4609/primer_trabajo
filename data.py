dfa9be4860dd1d9855a785be6069103c4d9be007
Isabel Sofia Hernandez <ishernandez100689@umanizales.edu.co>
2024-09-11 23:40:13 -0500

    last commit

--- data.py
+++ data.py
@@ -2,13 +2,13 @@ import mysql.connector
 import os
 import streamlit as st
 
-# Función para insertar datos en la base de datos en bloque
+
 def insert_data_in_bulk(df, table_name='empleados'):
     connection = None
     cursor = None
 
     try:
-        # Conectar a la base de datos usando las variables del entorno
+        
         connection = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
             user=os.getenv("DB_USER"),
@@ -20,17 +20,17 @@ def insert_data_in_bulk(df, table_name='empleados'):
             st.write("Connected to the database successfully.")
             cursor = connection.cursor()
 
-            # Consulta SQL para insertar datos en la tabla 
-            # la f para asignar valores
+            # 
+            
             insert_query = f"""
                 INSERT INTO {table_name} (Cod_Empleado, Empleado, Email, Area, Horario)
                 VALUES (%s, %s, %s, %s, %s)
                 """
 
-            # Convertir el DataFrame a una lista de tuplas para insertar
+            
             data = df[['Cod_Empleado', 'Empleado', 'Email', 'Area', 'Horario']].to_records(index=False).tolist()
 
-            # Ejecutar la consulta en bloque
+            
             cursor.executemany(insert_query, data)
             
             
@@ -43,7 +43,7 @@ def insert_data_in_bulk(df, table_name='empleados'):
         if connection:
             connection.rollback()
 
-# cierre de conexión
+
     finally:
         if cursor:
             cursor.close()
